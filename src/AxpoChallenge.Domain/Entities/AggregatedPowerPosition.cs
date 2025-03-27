4f00b89fea517dcff0390aff35ea107d20779b45
vikator88 <victor.lendinez@outlook.com>
2025-03-27 12:07:52 +0100

    refactor: ♻️ add Id to AggregatedPowerPosition to make it an actual DDD entity

--- src/AxpoChallenge.Domain/Entities/AggregatedPowerPosition.cs
+++ src/AxpoChallenge.Domain/Entities/AggregatedPowerPosition.cs
@@ -4,10 +4,12 @@ public sealed class AggregatedPowerPosition
 {
     public AggregatedPowerPosition(DateTime dateTimeUtc)
     {
+        Id = Guid.NewGuid();
         DateTimeUtc = dateTimeUtc;
         Volume = 0;
     }
 
+    public Guid Id { get; }
     public DateTime DateTimeUtc { get; }
     public double Volume { get; private set; }
 
